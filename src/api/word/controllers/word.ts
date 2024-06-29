abee23438655c5a7b5a0935301b74c6cd4e5b9ab
Abdul Aziz <sayaabdaziz1@gmail.com>
2024-06-29 14:46:57 +0700

    first commit

---
+++ src/api/word/controllers/word.ts
@@ -0,0 +1,7 @@
+/**
+ * word controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::word.word');
---
+++ src/api/word/services/word.ts
@@ -0,0 +1,7 @@
+/**
+ * word service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::word.word');
